c972f46057769dbf51cca10c453339a347b7ca01
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2025-09-01 22:20:03 -0600

    feat: add Windows MKL integration via NuGet
    Updated `build.rs` to support Windows MKL installation using NuGet. Improved logic for platform-specific MKL setup and made minor cleanup adjustments for consistency.

--- build.rs
+++ build.rs
@@ -10,58 +10,80 @@ use std::path::PathBuf;
 use std::process::Command;
 use std::{env, fs, io};
 
-const MKL_INSTALLER_URL: &str = "https://registrationcenter-download.intel.com/akdlm/IRC_NAS/47c7d946-fca1-441a-b0df-b094e3f045ea/intel-onemkl-2025.2.0.629.sh";
+const NUGET_PACKAGE_ID: &str = "intelmkl.static.win-x64";
+const LINUX_INSTALLER_URL: &str = "https://registrationcenter-download.intel.com/akdlm/IRC_NAS/47c7d946-fca1-441a-b0df-b094e3f045ea/intel-onemkl-2025.2.0.629.sh";
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
-    if cfg!(target_os = "macos") {
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let lib_dir = if cfg!(target_os = "macos") {
         panic!("MacOS is not supported")
     } else if cfg!(target_os = "windows") {
-        panic!("Windows is not supported yet")
-    }
+        let lib_dir = out_dir.join(NUGET_PACKAGE_ID).join("build/native/lib");
+        if !lib_dir.exists() {
+            let status = Command::new("nuget.exe")
+                .args([
+                    "install",
+                    NUGET_PACKAGE_ID,
+                    "-OutputDirectory",
+                    &out_dir.to_str().unwrap(),
+                    "-DirectDownload",
+                    "-ExcludeVersion",
+                ])
+                .status()
+                .unwrap();
+            assert!(
+                status.success(),
+                "Failed to install MKL with status: {:?}",
+                status.code().unwrap()
+            );
+            fs::rename(lib_dir.join("../win-x64"), &lib_dir).unwrap();
+        }
+        lib_dir
+    } else {
+        let lib_dir = out_dir.join("mkl/latest/lib");
+        if !lib_dir.exists() {
+            let res = ureq::get(LINUX_INSTALLER_URL)
+                .call()
+                .expect("Failed to fetch MKL installer");
+            assert_eq!(
+                res.status(),
+                200,
+                "Download failed with status {}",
+                res.status()
+            );
 
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let lib_dir = out_dir.join("mkl/latest/lib");
-    if !lib_dir.exists() {
-        let res = ureq::get(MKL_INSTALLER_URL)
-            .call()
-            .expect("Failed to fetch MKL installer");
-        assert_eq!(
-            res.status(),
-            200,
-            "Download failed with status {}",
-            res.status()
-        );
+            let installer_name = LINUX_INSTALLER_URL.split('/').last().unwrap();
+            io::copy(
+                &mut io::BufReader::new(res.into_body().into_reader()),
+                &mut fs::File::create(out_dir.join(installer_name)).unwrap(),
+            )
+            .unwrap();
 
-        let installer_name = MKL_INSTALLER_URL.split('/').last().unwrap();
-        io::copy(
-            &mut io::BufReader::new(res.into_body().into_reader()),
-            &mut fs::File::create(out_dir.join(installer_name)).unwrap(),
-        )
-        .unwrap();
+            let installation_args = vec![
+                installer_name,
+                "-a",
+                "-s",
+                "--eula",
+                "accept",
+                "--install-dir",
+                out_dir.to_str().unwrap(),
+            ];
 
-        let installation_args = vec![
-            installer_name,
-            "-a",
-            "-s",
-            "--eula",
-            "accept",
-            "--install-dir",
-            out_dir.to_str().unwrap(),
-        ];
-
-        let mut cmd = Command::new("sh");
-        cmd.current_dir(&out_dir);
-        cmd.args(&installation_args);
-        cmd.env("HOME", &out_dir);
-        let status = cmd.status().expect("Failed to run MKL installer");
-        assert!(
-            status.success(),
-            "Failed to install MKL with status: {:?}",
-            status.code().unwrap()
-        );
-    }
+            let mut cmd = Command::new("sh");
+            cmd.current_dir(&out_dir);
+            cmd.args(&installation_args);
+            cmd.env("HOME", &out_dir);
+            let status = cmd.status().expect("Failed to run MKL installer");
+            assert!(
+                status.success(),
+                "Failed to install MKL with status: {:?}",
+                status.code().unwrap()
+            );
+        }
+        lib_dir
+    };
 
     println!("cargo:rustc-link-search={}", lib_dir.display());
     if cfg!(feature = "lp64") {
@@ -75,6 +97,8 @@ fn main() {
         println!("cargo:rustc-link-lib=static=mkl_sequential");
     }
     println!("cargo:rustc-link-lib=static=mkl_core");
-    println!("cargo:rustc-link-lib=mkl_rt");
-    println!("cargo::metadata=ROOT={}", out_dir.join("mkl/latest").display());
+    println!(
+        "cargo::metadata=ROOT={}",
+        lib_dir.parent().unwrap().display()
+    );
 }
